cce1bdbe502d264ca518abffb8192099d44b4359
wolfblack27 <tigowolfblack27@gmail.com>
2023-10-29 20:01:07 -0300

    Criado main

--- src/Aplication/Main.java
+++ src/Aplication/Main.java
@@ -1,10 +1,31 @@
 package Aplication;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// 
 
+		System.out.println("******METHOD 2**********");
+		method2();
+
+	}
+
+	public static void method2() {
+		try {
+			Scanner sc = new Scanner(System.in);
+			String[] vetor = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.printf("O nome digitado foi:%s%n", vetor[position]);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}finally {
+			System.out.println("******Sempre executa**********");
+		}
+		
+
+		
 	}
 
 }
